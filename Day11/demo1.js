6a0ada9e5c8f42667c395903c9aa30c7d9c0b65a
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2021-12-16 20:34:20 +0530

    Day11

---
+++ Day11/demo1.js
@@ -0,0 +1,10 @@
+console.log("Hello World..");
+
+// document.write("jagate rahooo...");
+
+var a1=45;
+console.log('Value of a1  is '+a1);
+console.log("To check its type "+typeof a1)
+a1='Sumit';
+console.log('Value of a1  is '+a1);
+console.log("To check its type "+typeof a1)
